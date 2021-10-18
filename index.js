45fb4c44bbdb5662564364668f2df298bc5f23e4
ronin540 <satyamor2@gmail.com>
2021-10-18 20:49:26 +0530

    12th commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/subscribe", async (req, res) => {
 			console.log("Successfully subscribed to topic:", response);
 		});
 });
-app.post("/sendNotification", async (req, res) => {
+app.get("/sendNotification", async (req, res) => {
 	// Send a message to devices subscribed to the provided topic.
 	let topic = "sam";
 	let message = {
